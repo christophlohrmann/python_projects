0274581e80c515084e1c4308a2a9ee7f3f66dc0a
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-05-01 15:26:56 +0200

    subtype and old gains in main

--- gains_dashboard/main.py
+++ gains_dashboard/main.py
@@ -5,13 +5,42 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_daq as daq
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.subplots
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+def get_oldest_pr(df_, number):
+    # get the newest entry of all subtypes
+    df = df_.copy()
+    newest = pd.DataFrame()
+    for type in df['type'].unique():
+        df_type = df[df['type'] == type]
+        for subtype in df_type['subtype'].unique():
+            df_subtype = df_type[df_type['subtype'] == subtype]
+            df_subtype = df_subtype.sort_values(by='date', ascending=False)
+            newest = newest.append(df_subtype.iloc[0])
+    newest = newest.sort_values(by='date')
+    return newest.iloc[0:number]
+
+
+def generate_old_gain_table(df, max_rows = 5):
+    oldest = get_oldest_pr(df, max_rows)
+    display_cols = ['date', 'type', 'subtype']
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in display_cols])
+        ),
+        html.Tbody(
+            [html.Tr([html.Td(oldest.iloc[i][col]) for col in display_cols]) for i in range(max_rows)]
+        )])
+
+
+
+external_stylesheets =[dbc.themes.BOOTSTRAP] #['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -21,10 +50,14 @@ df = pd.read_pickle('./resources/the_gains.pick')
 all_types = df['type'].unique().tolist()
 all_goals = df['goal'].unique().tolist()
 
-user_wants = {'cat': all_types[0],
-              'goal': all_goals[0],
-              'group_by': all_goals[1],
+available_goals = dict()
+for type_ in all_types:
+    available_goals[type_] = df[df['type'] == type_]['goal'].unique().tolist()
+
+user_wants = {'type': all_types[0],
+              'goal': available_goals[all_types[0]],
               'relative': False}
+
 
 app.layout = html.Div(children=[
     html.H1(children='Welcome to the GAINZ dashboard'),
@@ -33,21 +66,12 @@ app.layout = html.Div(children=[
     dcc.Dropdown(
         id='dropdown_type',
         options=[{'label': type, 'value': type} for type in all_types],
-        value=user_wants['cat']
+        value=user_wants['type']
     ),
 
     html.Div('Choose goal'),
     dcc.Dropdown(
-        id='dropdown_goal',
-        options=[{'label': goal, 'value': goal} for goal in all_goals],
-        value=user_wants['goal']
-    ),
-
-    html.Div('Group by'),
-    dcc.Dropdown(
-        id='dropdown_group_by',
-        options=[{'label': goal, 'value': goal} for goal in all_goals],
-        value=user_wants['group_by']
+        id='dropdown_goal'
     ),
 
     dcc.Graph(
@@ -61,7 +85,10 @@ app.layout = html.Div(children=[
         value=user_wants['relative'],
         label='display relative increase',
         labelPosition='bottom'
-    )
+    ),
+
+    html.Div(children=[html.H4(children='Your top 5 oldest gains that could use some work'),
+                       generate_old_gain_table(df, max_rows=5)])
 ])
 
 y_labels = {'weight': 'weight in kg',
@@ -70,46 +97,52 @@ y_labels = {'weight': 'weight in kg',
             'goal_relative': 'relative performance increase'}
 
 
+@app.callback(dash.dependencies.Output('dropdown_goal', 'options'),
+              dash.dependencies.Input('dropdown_type', 'value'))
+def set_goal_options(type_):
+    return [{'label': goal, 'value': goal} for goal in available_goals[type_]]
+
+
+@app.callback(dash.dependencies.Output('dropdown_goal', 'value'),
+              dash.dependencies.Input('dropdown_goal', 'options'))
+def set_goal_default_value(options):
+    return options[0]['value']
+
+
 @app.callback(
     dash.dependencies.Output('graph_gain', 'figure'),
     [dash.dependencies.Input('dropdown_type', 'value'),
      dash.dependencies.Input('dropdown_goal', 'value'),
-     dash.dependencies.Input('dropdown_group_by', 'value'),
      dash.dependencies.Input('toggle_switch_relative', 'value')])
-def redraw_figure(dropdown_type, dropdown_goal, dropdown_group_by, toggle_rel):
-    user_wants['cat'] = dropdown_type
+def redraw_figure(dropdown_type, dropdown_goal, toggle_rel):
+    user_wants['type'] = dropdown_type
     user_wants['relative'] = toggle_rel
     user_wants['goal'] = dropdown_goal
-    user_wants['group_by'] = dropdown_group_by
     return draw_figure(df, user_wants)
 
 
 def draw_figure(df, user_wants):
-    type_ = user_wants['cat']
+    type_ = user_wants['type']
     goal = user_wants['goal']
-    group_by = user_wants['group_by']
     relative = user_wants['relative']
 
-    not_goal = all_goals.copy()
-    not_goal.remove(goal)
-
-    relevant_gains = df[df['type'] == type_].copy()
-    relevant_gains = relevant_gains[relevant_gains['goal'] == goal]
+    # find which subtypes are compatible with type and goal
+    df_filtered = df[df['type'] == type_].copy()
+    df_filtered = df_filtered[df_filtered['goal'] == goal]
 
     if relative:
         # split the dataframes according to the group_by value
-        group_vals = relevant_gains[group_by].unique()
-        grouped_dfs = [relevant_gains[relevant_gains[group_by] == group_val].copy() for group_val in group_vals]
-        relevant_gains = pd.DataFrame()
+        group_vals = df_filtered['subtype'].unique()
+        grouped_dfs = [df_filtered[df_filtered['subtype'] == group_val].copy() for group_val in group_vals]
+        df_filtered = pd.DataFrame()
         # perform the division and stitch the frame back together
         for grouped_df in grouped_dfs:
             grouped_df.sort_values(by='date')
             grouped_df['goal_relative'] = grouped_df[goal] / grouped_df[goal].iloc[0]
-            relevant_gains = relevant_gains.append(grouped_df, ignore_index=True)
+            df_filtered = df_filtered.append(grouped_df, ignore_index=True)
 
     goal = 'goal_relative' if relative else goal
-    fig = px.line(relevant_gains, x='date', y=goal, color=group_by, hover_data=not_goal,
-                  labels=y_labels)
+    fig = px.line(df_filtered, x='date', y=goal, color='subtype', labels=y_labels)
     fig.update_layout()
 
     return fig
