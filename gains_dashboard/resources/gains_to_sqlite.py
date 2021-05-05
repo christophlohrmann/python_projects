c0169e36c05a8507ecae65d02641e5bbab51f50c
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-05-05 19:34:48 +0200

    more proper subtype naming and handling

--- gains_dashboard/resources/gains_to_sqlite.py
+++ gains_dashboard/resources/gains_to_sqlite.py
@@ -1,6 +1,9 @@
 import sqlite3
 import pandas as pd
 
-df = pd.read_pickle('./the_gains.pick')
+df_gains = pd.read_pickle('./the_gains.pick')
+df_series = pd.read_pickle('./series.pick')
+
 conn = sqlite3.connect('the_gains.db')
-df.to_sql('the_gains', conn, if_exists = 'replace', index = False)
+df_gains.to_sql('the_gains', conn, if_exists ='replace', index = False)
+df_series.to_sql('series', conn, if_exists ='replace', index = False)
