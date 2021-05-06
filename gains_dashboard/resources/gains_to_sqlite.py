e05e476fb431a9f872cf02e666e1a8bce82ddfd5
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-05-06 16:03:40 +0200

    remove projects

--- gains_dashboard/resources/gains_to_sqlite.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-
-df_gains = pd.read_pickle('./the_gains.pick')
-df_series = pd.read_pickle('./series.pick')
-
-conn = sqlite3.connect('the_gains.db')
-df_gains.to_sql('the_gains', conn, if_exists ='replace', index = False)
-df_series.to_sql('series', conn, if_exists ='replace', index = False)
